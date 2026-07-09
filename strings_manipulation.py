319a8de01e28fcf218652499692ed9b07a335485
creatorMujih <mercyujih@gmail.com>
2026-07-09 23:50:04 +0100

    basics

---
+++ strings_manipulation.py
@@ -0,0 +1,14 @@
+#formatting strings
+age = 37
+txt = f"Joe is a boy, he is {age} years old"
+print(txt)
+
+#modifying strings
+#Making strings into upper case
+
+b = "Ali is a boy"
+print(b.upper())
+
+#Making strings into lower case
+m = "ADA IS A GIRL"
+print(m.lower())
